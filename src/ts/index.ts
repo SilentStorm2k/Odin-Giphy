9ab478e7a705a941bd509c9ea8a2f2041324d788
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2025-04-24 01:45:34 +0530

    simple script for showing gif

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,30 @@
 import '../css/styles.css';
+import defaultImage from '../assets/defaultImage.png';
+import { getGifUrl } from './api';
 
-console.log('hello world');
+const img = document.querySelector('img') as HTMLImageElement;
+const errorSpan = document.querySelector('span') as HTMLSpanElement;
+const searchButton = document.querySelector('button') as HTMLButtonElement;
+const searchInput = document.querySelector('input') as HTMLInputElement;
+
+searchButton.addEventListener('click', () => showImage(searchInput.value));
+searchInput.addEventListener('keypress', (e) => {
+    if (e.key == 'Enter') showImage(searchInput.value);
+});
+
+async function showImage(searchTopic: string) {
+    getGifUrl(searchTopic)
+        .then((url) => {
+            img.src = url;
+            errorSpan.textContent = '';
+            errorSpan.classList.remove('active');
+        })
+        .catch((errorMessage) => {
+            console.log(errorMessage);
+            img.src = defaultImage;
+            errorSpan.textContent = errorMessage;
+            errorSpan.classList.add('active');
+        });
+}
+
+showImage('cats');
